5964124fdd38571b9d815ec3e2886a8bfbf5dbbc
Jim Hague <jim@sinodun.com>
2017-09-29 12:01:54 +0100

    Add authentication to Stubby Manager helper.

--- osx/stubby-ui-helper.c
+++ osx/stubby-ui-helper.c
@@ -5,6 +5,7 @@
  * requires privileges.
  */
 
+#include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,9 +20,64 @@ static const char STUBBY_SETDNS[] = "/usr/local/sbin/stubby-setdns-macos.sh";
 static const char STUBBY[] = "/usr/local/bin/stubby";
 static const char DEFAULT_CONFIG_FILE[] = "/usr/local/etc/stubby/stubby.yml";
 
-void check_auth(const char *auth)
+static const char RIGHT_DAEMON_RUN[] = "net.getdnsapi.stubby.daemon.run";
+static const char RIGHT_DNS_LOCAL[] = "net.getdnsapi.stubby.dns.local";
+
+void check_auth(const char *auth, const char *right)
 {
-        return;
+        if (!auth) {
+                fprintf(stderr, "Authorization required.");
+                os_log(OS_LOG_DEFAULT, "Required authorization not supplied.");
+                exit(1);
+        }
+
+        AuthorizationExternalForm auth_ext_form;
+        for (size_t i = 0; i < kAuthorizationExternalFormLength; ++i) {
+                char c = 0;
+
+                if (isxdigit(*auth)) {
+                        char n = (*auth >= 'a') ? *auth + 10 - 'a' : *auth - '0';
+                        c |= (n << 4);
+                        ++auth;
+                        if (isxdigit(*auth)) {
+                                n = (*auth >= 'a') ? *auth + 10 - 'a' : *auth - '0';
+                                c |= n;
+                                ++auth;
+                                auth_ext_form.bytes[i] = c;
+                                continue;
+                        }
+                }
+                fprintf(stderr, "Invalid authorization key text.");
+                os_log(OS_LOG_DEFAULT, "Invalid authorization key test.");
+                exit(1);
+        }
+
+        AuthorizationRef auth_ref;
+        OSStatus oss;
+
+        oss = AuthorizationCreateFromExternalForm(&auth_ext_form, &auth_ref);
+        if (oss != errAuthorizationSuccess) {
+                fprintf(stderr, "Bad authorization key form.");
+                os_log(OS_LOG_DEFAULT, "Authorization key is of wrong form.");
+                exit(1);
+        }
+
+        AuthorizationItem one_right = { right, 0, NULL, 0 };
+        AuthorizationRights rights = { 1, &one_right };
+
+        oss = AuthorizationCopyRights(
+                auth_ref,
+                &rights,
+                kAuthorizationEmptyEnvironment,
+                kAuthorizationFlagExtendRights | kAuthorizationFlagInteractionAllowed,
+                NULL);
+        if (oss != errAuthorizationSuccess) {
+                fprintf(stderr, "Authorization declined.");
+                os_log(OS_LOG_DEFAULT, "Authorization declined.");
+                exit(1);
+        }
+
+        AuthorizationFree(auth_ref, kAuthorizationFlagDefaults);
 }
 
 void usage()
@@ -131,21 +187,21 @@ int main(int ac, char *av[])
                 fail_with_errno("setuid");
 
         if (strcmp(cmd, "start") == 0) {
-                check_auth(auth);
+                check_auth(auth, RIGHT_DAEMON_RUN);
                 start();
         }
         else if (strcmp(cmd, "stop") == 0) {
-                check_auth(auth);
+                check_auth(auth, RIGHT_DAEMON_RUN);
                 stop();
         }
         else if (strcmp(cmd, "list") == 0)
                 list();
         else if (strcmp(cmd, "dns_stubby") == 0) {
-                check_auth(auth);
+                check_auth(auth, RIGHT_DNS_LOCAL);
                 dns_stubby();
         }
         else if (strcmp(cmd, "dns_default") == 0) {
-                check_auth(auth);
+                check_auth(auth, RIGHT_DNS_LOCAL);
                 dns_default();
         }
         else if (strcmp(cmd, "dns_list") == 0)
