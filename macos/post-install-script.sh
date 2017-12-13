b7374cc5bda17623462d886bbe00b595e0df04da
Sara Dickinson <sara@sinodun.com>
2017-12-13 10:14:43 +0000

    Add post install script here

---
+++ macos/post-install-script.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+security authorizationdb write net.getdnsapi.stubby.daemon.run < /Applications/StubbyManager.app/Contents/MacOS/rights.daemon.run.plist
+security authorizationdb write net.getdnsapi.stubby.dns.local < /Applications/StubbyManager.app/Contents/MacOS/rights.dns.local.plist
+sudo chmod 6755 /Applications/StubbyManager.app/Contents/MacOS/stubby-ui-helper
