6c33e528f0cb2b92fa569d975bca5abf43ceb3d8
Sara Dickinson <sara@sinodun.com>
2017-12-13 10:45:51 +0000

    Need to touch log file for contents to be available on the very first run of the app

--- macos/post-install-script.sh
+++ macos/post-install-script.sh
@@ -3,3 +3,4 @@
 security authorizationdb write net.getdnsapi.stubby.daemon.run < /Applications/StubbyManager.app/Contents/MacOS/rights.daemon.run.plist
 security authorizationdb write net.getdnsapi.stubby.dns.local < /Applications/StubbyManager.app/Contents/MacOS/rights.dns.local.plist
 sudo chmod 6755 /Applications/StubbyManager.app/Contents/MacOS/stubby-ui-helper
+sudo touch /var/log/stubby.log
